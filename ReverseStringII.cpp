48a2cf1654cd859badc0569b4fee9f85a67e3fae
Bonnieliuliu <fangyuanliu@pku.edu.cn>
2018-07-24 10:06:31 +0800

    2018-07-22(1)

---
+++ ReverseStringII.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int n=s.size(), cnt = n/k;
+        for (int i=0; i<=cnt; ++i){
+            if (i%2==0){
+                if (i*k+k<n){
+                    reverse(s.begin()+i*k, s.begin()+i*k+k);
+                }
+                else{
+                    reverse(s.begin()+i*k, s.begin()+n);
+                }
+            }
+        }
+        return s;
+    }
+};
